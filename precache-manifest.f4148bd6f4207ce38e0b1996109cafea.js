e147ebc13f554e236674e7c6224a7354a11e5d77
Yahub <johndoe@example.com>
2019-09-03 19:35:48 +0300

    Updates

---
+++ precache-manifest.f4148bd6f4207ce38e0b1996109cafea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc7acf47adb220c33ee3fc3a5eec4316",
+    "url": "/notes/index.html"
+  },
+  {
+    "revision": "95c63d95cfc2089883e9",
+    "url": "/notes/static/css/main.35a04aa1.chunk.css"
+  },
+  {
+    "revision": "39581b82f7c2a6e78a73",
+    "url": "/notes/static/js/2.85cecbaa.chunk.js"
+  },
+  {
+    "revision": "95c63d95cfc2089883e9",
+    "url": "/notes/static/js/main.cd689c47.chunk.js"
+  },
+  {
+    "revision": "6e6216fc332e034d2c65",
+    "url": "/notes/static/js/runtime~main.bffad4ac.js"
+  }
+]);
